dc20db79179d906523189dcd22d7c9124b968adb
Hiram Rose <hiram.rosas@gmail.com>
2026-05-16 19:55:46 -0600

    User interface improvements and dark mode.

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,51 +5,53 @@ import { PaisService } from '../../services/pais.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-    `
-    li {
-      cursor: pointer;
-    }
-    `
-  ],
+  styles: [`li { cursor: pointer; }`],
   styleUrls: ['./por-pais.component.css']
 })
 export class PorPaisComponent {
 
-  termino: string = "";
-  hayError: boolean = false;
-  paises: Country[] = [];
-  paisesSugeridos: Country [] = [];
-  mostrarSugerencias: boolean = false;
-  
+  termino          = '';
+  hayError         = false;
+  cargando         = false;
+  paises: Country[]         = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias         = false;
+
   constructor(private paisService: PaisService) {}
 
   buscar(termino: string) {
-
     this.hayError = false;
-    console.log(this.termino);
-    this.termino = termino;
+    this.termino  = termino;
+    this.mostrarSugerencias = false;
+    this.cargando = true;
 
-    this.paisService.buscarPais(termino).subscribe(paises => {
-      console.log(paises);
-      this.paises = paises;
-      
-    }, (err) => {
-      this.hayError = true;
-      this.paises = [];
-    });
+    this.paisService.buscarPais(termino).subscribe(
+      paises => { this.paises = paises; this.cargando = false; },
+      ()     => { this.hayError = true; this.paises = []; this.cargando = false; }
+    );
   }
 
   sugerencias(termino: string) {
-    this.hayError = false;
-    this.termino = termino;
+    this.hayError  = false;
+    this.termino   = termino;
     this.mostrarSugerencias = true;
 
-    this.paisService.buscarPais(termino).subscribe(paises => this.paisesSugeridos = paises.splice(0,5),
-    (err) => this.paisesSugeridos = []);
+    this.paisService.buscarPais(termino).subscribe(
+      paises => this.paisesSugeridos = paises.slice(0, 5),
+      ()     => this.paisesSugeridos = []
+    );
   }
 
   buscarSugerido(termino: string) {
     this.buscar(termino);
+  }
+
+  limpiarBusqueda() {
+    this.paises             = [];
+    this.paisesSugeridos    = [];
+    this.mostrarSugerencias = false;
+    this.hayError           = false;
+    this.termino            = '';
+    this.cargando           = false;
   }
 }
